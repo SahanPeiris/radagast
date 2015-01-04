e8b78b0b9f33e92673907ea27d501f6f38544529
Sahan <sapeirisw@gmail.com>
2015-01-03 23:10:30 -0800

    fixed some blocking issues

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ var start = function(route, handle) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname +  " recieved");
 
-		route(pathname, handle);
-
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
-		response.end();
+		route(handle, pathname, response);
 	}
 	
 	http.createServer(onRequest).listen(8888);
