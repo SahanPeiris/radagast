ae32b0b55790798b8df4b15f0379514ef8c9e672
Sahan <sapeirisw@gmail.com>
2015-01-07 21:40:00 -0800

    used formidable to upload and display imgs

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,9 @@
 var exec = require("child_process").exec;
 var querystring = require("querystring");
 var fs = require("fs");
+var formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
 	console.log("Request handler 'start' was called.");
 
 	// exec("find /",
@@ -19,9 +20,9 @@ function start(response, postData) {
 		'<charset=UTF-8" />' +
 		'<head>' +
 		'<body>' +
-		'<form action="/upload" method="post">' +
-		'<textarea name="text" rows="20" cols="60"></textarea>'	+
-		'<input type="submit" value="Submit Text" />' +
+		'<form action="/upload" enctype="multipart/form-data" method="post">' +
+		'<input type="file" name="upload" multiple="multiple"/>' +
+		'<input type="submit" value="Upload file" />' +
 		'</form>' +
 		'</body>' +
 		'</html>';
@@ -31,14 +32,28 @@ function start(response, postData) {
 		response.end();
 	}
 
-function upload(response, postData) {
-	console.log("Request handler 'upload' was called."); 
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Post Data Sent " + querystring.parse(postData).text);
-	response.end();
+function upload(response, request) {
+	console.log("Request handler 'upload' was called.");
+	var form = new formidable.IncomingForm();
+	console.log("About to parse"); 
+
+	form.parse(request, function(error, fields, files) {
+		console.log("Parsing done");
+		/* problem if renaming an existing file" */
+		fs.rename(files.upload.path, "tmp/test.png", function(error) {
+			if(error) {
+				fs.unlink("tmp/test.png");
+				fs.rename(files.upload.path, "tmp/test.png");
+			}
+		});
+		response.writeHead(200, {"Content-Type" : "text/html"});
+		response.write("recieved image: <br/>");
+		response.write("<img src='/show' />");
+		response.end();
+	})
 }
 
-function show(response, postData) {
+function show(response) {
 	console.log("Request handler 'show' was called");
 	fs.readFile("tmp/test.png", "binary", function(error, file) {
 		if (error) {
